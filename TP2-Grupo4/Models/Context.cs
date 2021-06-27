074e6195bead82a2dbd4fbff4cf548b7df80ff5b
saul zarate <saul.zarate@davinci.edu.ar>
2021-06-27 18:58:09 -0300

    clase Credenciales agregada en Helpers

--- TP2-Grupo4/Models/Context.cs
+++ TP2-Grupo4/Models/Context.cs
@@ -14,7 +14,7 @@ namespace TP2_Grupo4.Models
         public Context() { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("server=localhost;user=root;database=inicio-proyecto;port=3306;password=", new MySqlServerVersion(new Version(8, 0, 11)));
+            optionsBuilder.UseMySql(Credenciales.GetConnectionString(), new MySqlServerVersion(new Version(8, 0, 11)));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
